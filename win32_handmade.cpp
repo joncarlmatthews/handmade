0f0913fcdf6a6b88101fd30b8d02915ac4166466
Jon Matthews <joncarlmatthews@gmail.com>
2016-12-03 11:31:02 +0000

    set up

--- win32_handmade.cpp
+++ win32_handmade.cpp
@@ -1,10 +1,73 @@
 #include <windows.h>
 
-int CALLBACK WinMain(HINSTANCE hInstance, 
-						HINSTANCE hPrevInstance, 
-						LPSTR lpCmdLine, 
-						int nCmdShow)
+/*
+typedef struct tagWNDCLASS {
+  UINT      style;
+  WNDPROC   lpfnWndProc;
+  int       cbClsExtra;
+  int       cbWndExtra;
+  HINSTANCE hInstance;
+  HICON     hIcon;
+  HCURSOR   hCursor;
+  HBRUSH    hbrBackground;
+  LPCTSTR   lpszMenuName;
+  LPCTSTR   lpszClassName;
+} WNDCLASS, *PWNDCLASS;
+
+WindowProc(
+  _In_ HWND   hwnd,
+  _In_ UINT   uMsg,
+  _In_ WPARAM wParam,
+  _In_ LPARAM lParam
+);
+*/
+/*
+LRESULT CALLBACK mainWindowCallback(Window, Message, wParam, lParam)
 {
-	MessageBox(NULL, "This is Handmade Hero", "Handmade Hero", MB_OK);
+	LRESULT result = 0;
+	switch(Message){
+
+		case WM_SIZE:
+		{
+			OutputDebugString("WM_SIZE");
+		} break;
+
+		case WM_DESTROY:
+		{
+			OutputDebugString("WM_SIZE");
+		} break;
+
+		case WM_CLOSE:
+		{
+			OutputDebugString("WM_SIZE");
+		} break;
+
+		case WM_ACTIVATEAPP:
+		{
+			OutputDebugString("WM_SIZE");
+		} break;
+
+		default:
+		{
+			OutputDebugString("default");
+			result = 
+		} break;
+	}
+
+	return result;
+}
+*/
+
+int CALLBACK WinMain(HINSTANCE Instance, 
+						HINSTANCE PrevInstance, 
+						LPSTR commandLine, 
+						int showCode)
+{
+	WNDCLASS WindowClass = {};
+	WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+	//WindowClass.lpfnWndProc = ;
+	WindowClass.hInstance = Instance;
+	WindowClass.lpszClassName = "handmadeHeroWindowClass";
+
 	return(0);
 }
