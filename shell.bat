3bd33bf1707a4f5398f81e849cbd827b0de60ca0
Jon Matthews <joncarlmatthews@gmail.com>
2017-09-29 20:17:31 +0100

    Update shell.bat

--- shell.bat
+++ shell.bat
@@ -2,6 +2,6 @@
 REM Turn off comments being outputted to the command line.^
 
 REM Call vcvarsall.bat so we can run "cl" from the command line.^
-REM cl is the VS build too for compiling C/C++ files^
+REM cl is the VS build tool for compiling C/C++ files^
 REM Pass the x64 flag so cl is set to compile in 64 bit mode^
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
