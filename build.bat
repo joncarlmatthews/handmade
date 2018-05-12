0ff38f203844f362aef30ee1d6a47cba35cefeac
Jon Matthews <joncarlmatthews@gmail.com>
2018-05-12 23:29:22 +0100

    set up

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 @echo off
 REM Turn off comments being outputted to the command line.^
 
-REM cl -Zi .\win32_handmade.cpp /link User32.lib /out:"Debug\Handmade Hero.exe"^
 mkdir build
-cl -Zi .\win32_handmade.cpp /link User32.lib /out:"build\win32_handmade_debug.exe"^
+cl -Zi .\win32_handmade.cpp /link User32.lib /out:"build\win32_handmade_debug.exe"
