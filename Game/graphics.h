6fb32dd012f87d73bad4fb9a7abc26a39d937e3b
Jon Matthews <joncarlmatthews@gmail.com>
2023-01-30 09:01:31 +0000

    Moving graphics code into separate files

---
+++ Game/graphics.h
@@ -0,0 +1,27 @@
+#ifndef HEADER_HH_GRAPHICS
+#define HEADER_HH_GRAPHICS
+
+#include "types.h"
+
+//
+// Graphics
+//====================================================
+
+/**
+* Write a rectangle into the frame buffer
+*
+* @param xOffset   Offset, in pixels along the x axis to start drawing from
+* @param yOffset   Offset, in pixels along the y axis to start drawing from
+* @param width     Width, in pixels, to draw
+* @param height    Height, in pixels, to draw
+* @return void
+*/
+typedef struct GameFrameBuffer GameFrameBuffer;
+void writeRectangle(GameFrameBuffer *buffer,
+                    int64 xOffset,
+                    int64 yOffset,
+                    int64 width,
+                    int64 height,
+                    Colour colour);
+
+#endif
