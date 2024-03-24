564d2abd2745a9117437daa2792d13c03db61a24
Jon Matthews <joncarlmatthews@gmail.com>
2024-03-24 20:49:04 +0000

    physics file for future vector implementation

---
+++ Game/physics.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_HH_PHYSICS
+#define HEADER_HH_PHYSICS
+
+//
+// PHYSICS.H
+//==============================================================================
+//
+
+// How many pixels to represent 1 meter in our game.
+#define PIXELS_PER_METER 20
+
+#endif
