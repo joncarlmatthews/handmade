a7b1fda8ece015f3e63dfd25d60b281576d5c6dd
Jon Matthews <joncarlmatthews@gmail.com>
2026-05-09 20:27:43 +0100

    xcode boilerplate

---
+++ Platform macOS/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Platform macOS
+//
+//  Created by Jon Matthews on 09/05/2026.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Platform macOS/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Platform macOS
+//
+//  Created by Jon Matthews on 09/05/2026.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
